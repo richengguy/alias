9a0f83a041d7300c949c5f9e5a9c692f086c3d2c
Richard Rzeszutek <richengguy@users.noreply.github.com>
2021-03-12 22:56:25 -0500

    Removed some commented out code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,13 +21,3 @@ def app(tmp_path) -> Flask:
 @pytest.fixture
 def client(app) -> FlaskClient:
     return app.test_client()
-
-    # app = create_app(tmp_path)
-    # app.config['TESTING'] = True
-
-    # with app.test_client() as client:
-    #     with app.app_context():
-    #         dbfile = tmp_path / app.config['DATABASE_NAME']
-    #         with LinksRegistry(dbfile) as registry:
-    #             registry.initialize()
-    #     yield client
